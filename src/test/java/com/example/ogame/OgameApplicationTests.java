7c035aa0d76b2e7e0f2ca5a924d86c410617732d
przemo06133 <przemo06133@gmail.com>
2019-08-18 20:03:24 +0200

    Init

---
+++ src/test/java/com/example/ogame/OgameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ogame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OgameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
