5fadb6f5e91634b61869354c16b1bc6b22eebaaa
przemo06133 <przemo06133@gmail.com>
2020-01-03 13:06:36 +0100

    test user service

--- src/test/java/com/example/ogame/services/UserServiceTest.java
+++ src/test/java/com/example/ogame/services/UserServiceTest.java
@@ -1,8 +1,8 @@
 package com.example.ogame.services;
 
-import com.example.ogame.datasource.BuildingDataAccess;
 import com.example.ogame.datasource.UserDataAccess;
 import com.example.ogame.datasource.VerifyDataAccess;
+import com.example.ogame.exeptions.ApiRequestException;
 import com.example.ogame.models.User;
 import org.hamcrest.Matchers;
 import org.junit.After;
@@ -13,11 +13,9 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -29,18 +27,20 @@ public class UserServiceTest {
     @Mock
     UserDataAccess userDataAccess;
 
-    @Mock
-    BuildingDataAccess buildingDataAccess;
-
-    @Mock
-    ApplicationUserDao applicationUserDao;
-
     @InjectMocks
     UserService userService;
 
+    User user;
+
     @Before
     public void setUp() throws Exception {
+
         given(userService.getAllUsers()).willReturn(getUserList());
+
+        given(verifyDataAccess.ifThisUserExists("user", "123")).willReturn(true);
+
+        given(userService.getUserByUsernamePassword("user", "123"))
+                .willReturn(user);
     }
 
     @After
@@ -53,13 +53,26 @@ public class UserServiceTest {
     }
 
     @Test
-    public void shouldCreateNerUser() {
-//        userService.createNewUser()
+    public void shouldGetUserByUsernamePassword() {
+        Assert.assertThat(userService.getUserByUsernamePassword("user", "123"), Matchers.is(user));
+    }
+
+    @Test
+    public void shouldNotGetUserByUsernamePassword() {
+        given(verifyDataAccess.ifThisUserExists("nonExistUser", "123")).willReturn(true);
+        User nonExistUser = new User("nonExistUser", "123", "user@email.com");
+        Assert.assertThat(userService.getUserByUsernamePassword("nonExistUser", "123"), Matchers.not(nonExistUser));
+    }
+
+    @Test(expected = ApiRequestException.class)
+    public void shouldThrowApiRequestException() {
+        given(verifyDataAccess.ifThisUserExists("nonExistUser", "123")).willReturn(false);
+        Assert.assertThat(userService.getUserByUsernamePassword("nonExistUser", "123"), Matchers.not(user));
     }
 
     public List<User> getUserList() {
         return Arrays.asList(
-                new User("user", "123", "user@mail.com"),
+                user,
                 new User("user2", "123", "user2@mail.com"),
                 new User("user3", "123", "user3@mail.com")
         );
