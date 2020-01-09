8635549e839de81cfc3de6fb7e123d8f86402ce9
przemo06133 <przemo06133@gmail.com>
2020-01-09 20:42:51 +0100

    getShipRowMapper added

--- src/main/java/com/example/ogame/datasource/FleetDataAccess.java
+++ src/main/java/com/example/ogame/datasource/FleetDataAccess.java
@@ -2,11 +2,12 @@ package com.example.ogame.datasource;
 
 import com.example.ogame.models.fleet.Fleet;
 import com.example.ogame.models.fleet.Ship;
-import com.example.ogame.utils.FleetHelper;
-import com.example.ogame.utils.ShipName;
+import com.example.ogame.utils.fleet.FleetHelper;
+import com.example.ogame.utils.facilities.ShipName;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -54,19 +55,23 @@ public class FleetDataAccess {
         return jdbcTemplate.queryForObject(
                 sql,
                 new Object[] {ship_id},
-                (rs, i) -> new Ship(
-                        UUID.fromString(rs.getString("ship_id")),
-                        Enum.valueOf(ShipName.class, rs.getString("ship_name")),
-                        rs.getInt("attack"),
-                        rs.getInt("defense"),
-                        rs.getInt("speed"),
-                        rs.getInt("capacity"),
-                        rs.getInt("fuel"),
-                        rs.getInt("metal_cost"),
-                        rs.getInt("cristal_cost"),
-                        rs.getInt("deuterium_cost"),
-                        rs.getInt("amount_of_ship")
-                ));
+                getShipRowMapper()
+        );
+    }
+
+    private RowMapper<Ship> getShipRowMapper() {
+        return (rs, i) -> new Ship(
+                UUID.fromString(rs.getString("ship_id")),
+                Enum.valueOf(ShipName.class, rs.getString("ship_name")),
+                rs.getInt("attack"),
+                rs.getInt("defense"),
+                rs.getInt("speed"),
+                rs.getInt("capacity"),
+                rs.getInt("fuel"),
+                rs.getInt("metal_cost"),
+                rs.getInt("cristal_cost"),
+                rs.getInt("deuterium_cost"),
+                rs.getInt("amount_of_ship"));
     }
 
     private List<UUID> getShipIds(UUID fleet_id) {
