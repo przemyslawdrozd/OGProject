cc0fa3096b19e9c0eca105ca98b0bf0b3f59bad0
przemo06133 <przemo06133@gmail.com>
2020-01-11 11:50:52 +0100

    Technology model created

---
+++ src/main/java/com/example/ogame/models/researches/TechName.java
@@ -0,0 +1,23 @@
+package com.example.ogame.models.researches;
+
+public class TechName {
+
+    public static final String ENERGY_TECH = "Energy Technology";
+    public static final String LASER_TECH = "Laser Technology";
+    public static final String ION_TECH = "Ion Technology";
+    public static final String HYPER_TECH = "Hyperspace Technology";
+    public static final String PLASMA_TECH = "Plasma Technology";
+    public static final String COMBUSTION_DRIVE = "Combustion Drive";
+    public static final String IMPULSIVE_DRIVE = "Impulse Drive";
+    public static final String HYPERSPACE_DRIVE = "Hyperspace Drive";
+    public static final String SPY_TECH = "Espionage Technology";
+    public static final String COMP_TECH = "Computer Technology";
+    public static final String ASTRO_TECH = "Astrophysics";
+    public static final String INTER_RESEARCH_TECH = "Intergalactic Research Network";
+    public static final String GRAVITON_TECH = "Graviton Technology";
+    public static final String WEP_TECH = "Weapons Technology";
+    public static final String SHIELD_TECH = "Shielding Technology";
+    public static final String ARMOR_TECH = "Armor Technology";
+
+    private TechName() {}
+}
---
+++ src/main/java/com/example/ogame/models/researches/Technology.java
@@ -0,0 +1,122 @@
+package com.example.ogame.models.researches;
+
+import org.apache.commons.lang3.time.DurationFormatUtils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.UUID;
+
+public class Technology {
+
+    private UUID shipId;
+    private String name;
+    private int lvl;
+    private int neededMetal;
+    private int neededCristal;
+    private int neededDeuterium;
+    private String buildTime;
+    private boolean isAbleToBuild;
+
+    public Technology(String name, int lvl, int neededMetal, int neededCristal,
+                      int neededDeuterium, String buildTime, boolean isAbleToBuild) {
+        this.name = name;
+        this.lvl = lvl;
+        this.neededMetal = neededMetal;
+        this.neededCristal = neededCristal;
+        this.neededDeuterium = neededDeuterium;
+        this.buildTime = buildTime;
+        this.isAbleToBuild = isAbleToBuild;
+    }
+
+    public long getDuration() {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("hh:mm:ss",
+                    Locale.getDefault());
+            dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+            return dateFormat.parse(buildTime).getTime();
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    private void setNewTime(long newTime) {
+        this.buildTime = DurationFormatUtils.formatDurationHMS(newTime);
+    }
+
+    public void lvlUpTech() {
+        lvl++;
+        this.neededMetal *= 2;
+        this.neededCristal *= 2;
+        this.neededDeuterium *= 2;
+        long newTime = getDuration() * 2;
+        setNewTime(newTime);
+    }
+
+    public String getBuildTime() {
+        return buildTime;
+    }
+
+    public UUID getShipId() {
+        return shipId;
+    }
+
+    public void setShipId(UUID shipId) {
+        this.shipId = shipId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNeededMetal() {
+        return neededMetal;
+    }
+
+    public void setNeededMetal(int neededMetal) {
+        this.neededMetal = neededMetal;
+    }
+
+    public int getNeededCristal() {
+        return neededCristal;
+    }
+
+    public void setNeededCristal(int neededCristal) {
+        this.neededCristal = neededCristal;
+    }
+
+    public int getNeededDeuterium() {
+        return neededDeuterium;
+    }
+
+    public void setNeededDeuterium(int neededDeuterium) {
+        this.neededDeuterium = neededDeuterium;
+    }
+
+    public void setBuildTime(String buildTime) {
+        this.buildTime = buildTime;
+    }
+
+    public boolean isAbleToBuild() {
+        return isAbleToBuild;
+    }
+
+    public void setAbleToBuild(boolean ableToBuild) {
+        isAbleToBuild = ableToBuild;
+    }
+
+    @Override
+    public String toString() {
+        return "Technology{" +
+                "name='" + name + '\'' +
+                ", lvl=" + lvl +
+                ", buildTime='" + buildTime + '\'' +
+                '}';
+    }
+}
