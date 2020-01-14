06eeb5f1aa6a1a537d76fd7edb7eb1299ffce3e2
przemo06133 <przemo06133@gmail.com>
2020-01-14 16:12:27 +0100

    galaxy table added

---
+++ src/main/resources/db/migration/V6__CreateGalaxyPosition.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS galaxy (
+
+   planet_id UUID NOT NULL PRIMARY KEY,
+   galaxy_position INTEGER NOT NULL,
+   planetary_system INTEGER NOT NULL,
+   planet_position INTEGER NOT NULL
+);
+
+ALTER TABLE user_instance
+ADD COLUMN planet_id UUID
+REFERENCES galaxy(planet_id),
+ADD UNIQUE (planet_id);
