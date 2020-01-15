a245509491b195018d48e8b82388a35d2a4af597
przemo06133 <przemo06133@gmail.com>
2020-01-15 23:04:47 +0100

    Planet controller added

---
+++ src/main/java/com/example/ogame/controllers/PlanetController.java
@@ -0,0 +1,38 @@
+package com.example.ogame.controllers;
+
+import com.example.ogame.models.Planet;
+import com.example.ogame.services.PlanetService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/planet-api")
+public class PlanetController {
+    private final Logger logger = LoggerFactory.getLogger(PlanetController.class);
+
+    private PlanetService planetService;
+
+    @Autowired
+    public PlanetController(PlanetService planetService) {
+        this.planetService = planetService;
+    }
+
+    @GetMapping
+    public List<Planet> getPlanets(){
+        return planetService.getPlanets();
+    }
+
+    @GetMapping("/{id}")
+    public Planet getPlanet(@PathVariable("id") String id) {
+        UUID planetId = UUID.fromString(id);
+        return planetService.getPlanet(id);
+    }
+
+}
